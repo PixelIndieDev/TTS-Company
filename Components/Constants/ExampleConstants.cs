e4a19b8948127188b449d72237cebced2561eb29
Pixel <89741457+PixelIndieDev@users.noreply.github.com>
2026-07-10 11:20:14 +0200

    Remove comment from exampleconstants

--- Components/Constants/ExampleConstants.cs
+++ Components/Constants/ExampleConstants.cs
@@ -1,6 +1,5 @@
 ﻿namespace TTSCompany.Components.Constants
 {
-    // for copy reasons
     internal static class ExampleConstants
     {
         internal const string VOICE_MODEL_NAME = "en_US-hfc_female-medium";
